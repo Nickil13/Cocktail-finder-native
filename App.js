2f5140e96c7c8f1f13176b153a6014df462cd9a5
Nicki Lindstrom <lindstromn13@gmail.com>
2023-03-17 17:28:00 -0600

    Initialization

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+const PlaceholderImage = require("./assets/images/cat.jpg");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Image source={PlaceholderImage} style={styles.image} />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
+    },
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    },
 });
